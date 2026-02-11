6a347fdd741810d0ce05bb943b9c5bd73fcfbb72
tlhomphogit <tlhomphosuper@gmail.com>
2026-02-11 15:22:36 +0200

    Implement expense reading functionality and enhance expense tracking display

--- e_tracker.py
+++ e_tracker.py
@@ -1,8 +1,21 @@
 import argparse
+import csv
 
 class Expense:
     def __init__(self):
-        expense = []
+        self.expense = []
 
     def read_from_file(self, filepath):
-        pass
+        vision = []
+        with open(filepath, mode='r') as file:
+            expense_data = csv.DictReader(file)
+
+        for row in expense_data:
+            self.expense.append(row)
+
+    def __str__(self):
+        return f'{self.expense}'
+
+    
+expense = Expense()
+print(expense.read_from_file('/home/wethinkcode/Documents/WeThinkCode/expense-tracker/expenses.csv'))
